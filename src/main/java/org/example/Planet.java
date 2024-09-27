cb1746f7e09471962ecda83df40a6863aaec893a
tempfantokhin <umbilicalpeanut@flaqua.site>
2024-09-27 14:08:20 +0300

    refactor: Add CosmicBody superclass
    - Add CosmicBody
    - Change Planet to extend CosmicBody

--- src/main/java/org/example/Planet.java
+++ src/main/java/org/example/Planet.java
@@ -1,46 +1,7 @@
 package org.example;
 
-/**
- * Represents a celestial body in our solar system.
- */
-public class Planet {
-    //The mass of the planet.
-    double mass;
-    //The name of the planet.
-    String name;
-    //An array of Satellite objects associated with this planet.
-    Satellite[] satellites;
-
-    /**
-     * Returns a string representation of the Planet object.
-     *
-     * @return A formatted string containing the planet's mass and name.
-     */
-    @Override
-    public String toString() {
-        return "planet[m=" + mass + ",n=" + name + "]";
-    }
-
-    /**
-     * Constructs a new Planet object.
-     *
-     * @param mass The mass of the planet.
-     * @param name The name of the planet.
-     * @param satellites An array of Satellite objects associated with this planet.
-     */
+public class Planet extends CosmicBody {
     public Planet(double mass, String name, Satellite[] satellites) {
-        this.mass = mass;
-        this.name = name;
-        this.satellites = satellites;
-    }
-
-    /**
-     * Gets the number of satellites associated with this planet.
-     *
-     * @return The number of satellites if any, otherwise 0.
-     */
-    public int getSatelliteCount() {
-        if (satellites == null) return 0;
-        return satellites.length;
+        super(mass, name, satellites);
     }
 }
